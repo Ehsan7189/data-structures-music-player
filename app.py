ccae8fd262263ec2f030e8386ae81473cba6c65f
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-06-30 21:53:16 +0330

    test for current_song

--- app.py
+++ app.py
@@ -1,16 +1,11 @@
 from structures.playlist import Playlist
 from models.song import Song
 
+# playlist = Playlist()
+
 playlist = Playlist()
 
 playlist.add_last(Song("music/Eminem - Mockingbird.mp3"))
 playlist.add_last(Song("music/Adele - Hello.mp3"))
-playlist.add_last(Song("music/Linkin Park - Numb.mp3"))
 
-print("=== قبل از حذف ===")
-playlist.display()
-
-playlist.delete("Hello")
-
-print("\n=== بعد از حذف ===")
-playlist.display()
+playlist.current_song()
