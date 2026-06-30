e5bd5286f8b456234dd5782b6bd673f581486558
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-06-30 22:19:17 +0330

    add measure for timing

--- algorithms/sorter.py
+++ algorithms/sorter.py
@@ -8,4 +8,15 @@ class Sorter:
 
         self.algorithm_name = algorithm_name
 
-    
+    def measure(self, playlist, key):
+        start_time = time.perf_counter()
+        tracemalloc.start()
+        self.sort(playlist, key)
+        current, peak = tracemalloc.get_traced_memory()
+        tracemalloc.stop()
+        end_time = time.perf_counter()
+        return {
+            "algorithm": self.algorithm_name,
+            "time": end_time - start_time,
+            "memory": peak
+        }
