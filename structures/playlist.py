e5b7b58eef12e7566ea56468fa8c99b94ed72537
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-06-30 13:27:36 +0330

    add add_first in playlist for add to first place of list

--- structures/playlist.py
+++ structures/playlist.py
@@ -7,3 +7,14 @@ class Playlist:
         self.head = None # * first
         self.tail = None # * current 
         self.current = None #  * last 
+    def add_first(self, song: Song):
+        new_node = Node(song)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.current = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+            
