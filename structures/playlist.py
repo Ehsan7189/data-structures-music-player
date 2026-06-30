83fc081a27420294cba6550419be20268a3fe15f
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-06-30 14:15:16 +0330

    defain display methode for show information of song

--- structures/playlist.py
+++ structures/playlist.py
@@ -17,4 +17,28 @@ class Playlist:
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
-            
+    def add_last(self, song: Song):
+        new_node = Node(song)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+            self.current = new_node
+        else:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+
+    def display(self):
+
+        if self.head is None:
+            print("Playlist is empty.")
+            return
+
+        current = self.head
+
+        while current is not None:
+
+            print(current.song)
+            print("-" * 30)
+
+            current = current.next
