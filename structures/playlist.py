bf300a65cdc21d78b637f31212fbb3ebc00c739d
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-07-03 19:49:30 +0330

    add size attribute in playlist

--- structures/playlist.py
+++ structures/playlist.py
@@ -7,6 +7,8 @@ class Playlist:
         self.head = None # * first
         self.tail = None # * current 
         self.current = None #  * last 
+        self.size = 0
+
     def add_first(self, song: Song):
         new_node = Node(song)
         if self.head is None:
@@ -17,6 +19,7 @@ class Playlist:
             new_node.next = self.head
             self.head.prev = new_node
             self.head = new_node
+        self.size += 1
     def add_last(self, song: Song):
         new_node = Node(song)
         if self.head is None:
@@ -27,6 +30,7 @@ class Playlist:
             new_node.prev = self.tail
             self.tail.next = new_node
             self.tail = new_node
+        self.size += 1
 
     def display(self):
 
