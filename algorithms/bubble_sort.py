9cf960521be287f95fd6bc878a2016eccd605552
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-07-03 12:47:05 +0330

    add sort method and add functionality of bubble sort

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -6,3 +6,18 @@ class BubbleSort(Sorter):
     def __init__(self):
 
         super().__init__("Bubble Sort")
+    def sort(self, playlist, key):
+        if playlist.head is None or playlist.head.next is None: 
+            return
+        swapped = True
+        while swapped:
+            swapped = False
+            current = playlist.head
+            while current.next:
+                left = getattr(current.song, key)
+                right = getattr(current.next.song, key)
+                if left > right:
+                    current.song, current.next.song = current.next.song, current.song
+                    swapped = True
+
+                current = current.next
