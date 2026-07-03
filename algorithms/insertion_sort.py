47bfc9d78fd2aca13b6e3c78a98919db46265849
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-07-03 19:37:20 +0330

    add sort method to d insertions sort funtionality

---
+++ algorithms/insertion_sort.py
@@ -0,0 +1,32 @@
+from algorithms.sorter import Sorter
+
+
+class InsertionSort(Sorter):
+
+    def __init__(self):
+        super().__init__("Insertion Sort")
+
+    def sort(self, playlist, key):
+        if playlist.head is None or playlist.head.next is None:
+            return
+
+        current = playlist.head.next
+        while current:
+            temp = current.song
+            position = current.prev
+
+            while position and self.compare(position, temp, key) > 0:
+                
+                position.next.song = position.song
+
+                position = position.prev
+        if position is None:
+
+            playlist.head.song = temp
+
+        else:
+
+            position.next.song = temp
+
+        current = current.next
+
