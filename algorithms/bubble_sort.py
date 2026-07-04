391b4c7c5f167e2b57cd575dc42559e63a4d1ddd
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-07-04 12:59:13 +0330

    some optimazation for bubble sorts better performance

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -6,31 +6,36 @@ class BubbleSort(Sorter):
     def __init__(self):
 
         super().__init__("Bubble Sort")
+        
 
-        print(self.__dict__)
     def sort(self, playlist, key):
         if playlist.head is None or playlist.head.next is None: 
             return
         swapped = True
         last_sorted = None
+
+        result = self.compare(current, current.next, key)
+
         while swapped:
             swapped = False
             current = playlist.head
 
             while current.next != last_sorted:
 
-                if self.compare(current, current.next, key) > 0:
+                if result > 0:
 
                     left = getattr(current.song, key)
                     right = getattr(current.next.song, key)
 
-                    if self.compare(current, current.next, key) > 0:
-
+                    if result > 0:
                         self.swap(current, current.next)
 
                         self.show_step(
                             playlist,
-                            f"Compare : {left} ↔ {right}\nResult  : Swap"
+                            key,
+                            left,
+                            right,
+                            "Swap"
                         )
 
                         swapped = True
@@ -39,9 +44,11 @@ class BubbleSort(Sorter):
 
                         self.show_step(
                             playlist,
-                            f"Compare : {left} ↔ {right}\nResult  : No Swap"
+                            key,
+                            left,
+                            right,
+                            "No Swap"
                         )
-
                     
 
                     self.swap(current, current.next)
