f351b07e44c5280663bc3e45a3a39a766d9f703d
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-07-04 13:06:05 +0330

    some optimization

--- algorithms/bubble_sort.py
+++ algorithms/bubble_sort.py
@@ -9,57 +9,47 @@ class BubbleSort(Sorter):
         
 
     def sort(self, playlist, key):
-        if playlist.head is None or playlist.head.next is None: 
-            return
+        if playlist.head is None or playlist.head.next is None:
+                return
+
         swapped = True
         last_sorted = None
 
-
         while swapped:
+
             swapped = False
             current = playlist.head
 
             while current.next != last_sorted:
 
-                if self.compare(current, current.next, key) > 0:
+                left = getattr(current.song, key)
+                right = getattr(current.next.song, key)
 
-                    left = getattr(current.song, key)
-                    right = getattr(current.next.song, key)
+                compare_result = self.compare(current, current.next, key)
 
-                    compare_result = self.compare(current, current.next, key)
-
-                    if compare_result > 0:
-                        self.swap(current, current.next)
-
-                        self.show_step(
-                            playlist,
-                            key,
-                            left,
-                            right,
-                            "Swap"
-                        )
-
-                        swapped = True
-
-                    else:
-
-                        self.show_step(
-                            playlist,
-                            key,
-                            left,
-                            right,
-                            "No Swap"
-                        )
-                    
+                if compare_result > 0:
 
                     self.swap(current, current.next)
 
                     self.show_step(
                         playlist,
-                        f"Compare : {left} ↔ {right}\nResult  : Swap"
+                        key,
+                        left,
+                        right,
+                        "Swap"
                     )
 
                     swapped = True
+
+                else:
+
+                    self.show_step(
+                        playlist,
+                        key,
+                        left,
+                        right,
+                        "No Swap"
+                    )
 
                 current = current.next
 
