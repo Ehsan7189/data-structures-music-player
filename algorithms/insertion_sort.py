26708d44a362c6e965b0705a7851e0a6911f3cde
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-07-05 12:26:45 +0330

    fix scop bug

--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -7,27 +7,26 @@ class InsertionSort(Sorter):
         super().__init__("Insertion Sort")
 
     def sort(self, playlist, key):
+
         if playlist.head is None or playlist.head.next is None:
             return
 
         current = playlist.head.next
+
         while current:
+
             temp = current.song
             next_node = current.next
             position = current.prev
 
             while position and self.compare(position, temp, key) > 0:
-                
+
                 position.next.song = position.song
-
                 position = position.prev
-        if position is None:
 
-            playlist.head.song = temp
+            if position is None:
+                playlist.head.song = temp
+            else:
+                position.next.song = temp
 
-        else:
-
-            position.next.song = temp
-
-        current = next_node
-
+            current = next_node
