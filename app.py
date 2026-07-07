81b1c46d0883860791e35f7ad733baadde0c3f3e
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-07-07 12:37:42 +0330

    define show menu un app

---
+++ app.py
@@ -0,0 +1,19 @@
+
+
+
+
+def show_menu():
+
+    print("\n" + "=" * 35)
+    print("        MUSIC PLAYER")
+    print("=" * 35)
+    print("1. Display Playlist")
+    print("2. Play")
+    print("3. Pause")
+    print("4. Stop")
+    print("5. Next")
+    print("6. Previous")
+    print("7. Toggle Shuffle")
+    print("8. Sort Playlist")
+    print("9. Show Performance")
+    print("0. Exit")
