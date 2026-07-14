ee2873e2518086aa70b3ad255a7c20d711616178
Ehsan7189 <ehsansayadi7189@gmail.com>
2026-07-14 13:37:06 +0330

    fix some bugs in music player

--- player/music_player.py
+++ player/music_player.py
@@ -1,33 +1,122 @@
-import os
-from models.song import Song
+import random
 
-class MusicLoader:
+class Player:
 
-    SUPPORTED_FORMATS = (".mp3", ".wav", ".flac", ".aac", ".ogg")
+    def __init__(self, playlist):
+        self.playlist = playlist
+        self.state = "Stopped"
+        self.shuffle_mode = False
+        self.shuffle_order = []
+        self.shuffle_index = 0
 
-    @staticmethod
-    def load_folder(path, playlist):
+    def _play_current_song(self, message):
+        song = self.playlist.current_song()
 
-        if not os.path.isdir(path):
-            print("\n❌ Folder not found.")
-            return 0
+        if not song:
+            print("Playlist is empty.")
+            return
 
-        count = 0
+        self.state = "Playing"
+        print(f"\n{message}")
+        print(song)
 
-        for file_name in os.listdir(path):
+    def play(self):
 
-            full_path = os.path.join(path, file_name)
+        if self.playlist.head is None:
+            print("Playlist is empty.")
+            return
 
-            if not os.path.isfile(full_path):
-                continue
+        if self.state == "Playing":
+            return
 
-            _, extension = os.path.splitext(file_name)
+        if self.state == "Paused":
+            self._play_current_song("▶ Resumed")
+            return
 
-            if extension.lower() not in MusicLoader.SUPPORTED_FORMATS:
-                continue
+        self._play_current_song("▶ Now Playing")
 
-            song = Song(full_path)
-            playlist.append(song)
-            count += 1
+    def pause(self):
 
-        return count
+        if self.state == "Playing":
+            self.state = "Paused"
+            print("⏸ Paused")
+
+    def stop(self):
+
+        if self.playlist.head is None:
+            return
+
+        self.state = "Stopped"
+        self.playlist.current = self.playlist.head
+
+        if self.shuffle_mode and self.shuffle_order:
+     
+            try:
+                self.shuffle_index = self.shuffle_order.index(self.playlist.head)
+            except ValueError:
+              
+                self._build_shuffle_order()
+                self.shuffle_index = 0
+       
+
+        print("\n⏹ Stopped")
+
+    def next(self):
+
+        if self.state == "Stopped":
+            return
+
+        if self.shuffle_mode:
+            if not self.shuffle_order:
+                return
+            self.shuffle_index = (self.shuffle_index + 1) % len(self.shuffle_order)
+            self.playlist.current = self.shuffle_order[self.shuffle_index]
+        else:
+            self.playlist.next_song()
+
+        self._play_current_song("⏭ Next Song")
+
+    def previous(self):
+
+        if self.state == "Stopped":
+            return
+
+        if self.shuffle_mode:
+            if not self.shuffle_order:
+                return
+            self.shuffle_index = (self.shuffle_index - 1) % len(self.shuffle_order)
+            self.playlist.current = self.shuffle_order[self.shuffle_index]
+        else:
+            self.playlist.previous_song()
+
+        self._play_current_song("⏮ Previous Song")
+
+    def shuffle(self):
+
+       
+        if self.playlist.head is None:
+            print("Playlist is empty. Cannot enable shuffle.")
+            return
+        
+
+        self.shuffle_mode = not self.shuffle_mode
+
+        if self.shuffle_mode:
+            self._build_shuffle_order()
+            self.playlist.current = self.shuffle_order[0]
+            self._play_current_song("🔀 Shuffle On")
+        else:
+            print("➡ Shuffle Off")
+
+    def _build_shuffle_order(self):
+
+        self.shuffle_order = []
+
+        current = self.playlist.head
+
+        while current:
+            self.shuffle_order.append(current)
+            current = current.next
+
+        random.shuffle(self.shuffle_order)
+        self.shuffle_index = 0
